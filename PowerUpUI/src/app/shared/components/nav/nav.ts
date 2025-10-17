8a0e55b528a522968e5ed09fc0ca2472984d662e
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-10-17 16:57:05 +0100

    feat(navigation): Implement nav component

---
+++ PowerUpUI/src/app/shared/components/nav/nav.ts
@@ -0,0 +1,13 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './nav.html',
+  styleUrls: ['./nav.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Nav {}
