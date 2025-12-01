7d7db4be371c38edf147bbbd5705ab0c6791ded3
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-01 19:16:50 +0000

    feat(Subscriptions): Enhance CreateSubscription method with validation and DTO usage

--- PowerUpAPI/PowerUp/Controllers/SubscriptionsController.cs
+++ PowerUpAPI/PowerUp/Controllers/SubscriptionsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PowerUp.Data;
 using PowerUp.Models;
+using PowerUp.Models.DTO;
 using Microsoft.AspNetCore.Authorization;
 
 namespace PowerUp.Controllers;
@@ -43,14 +44,34 @@ public class SubscriptionsController : ControllerBase
 
     // Create subscription
     [HttpPost]
-    public async Task<ActionResult<Subscription>> CreateSubscription(Subscription subscription)
+    public async Task<ActionResult<Subscription>> CreateSubscription([FromBody] CreateSubscriptionRequest request)
     {
+        // Validate enum
+        if (!Enum.TryParse<SubscriptionType>(request.Type, ignoreCase: true, out var subscriptionType))
+        {
+            return BadRequest(new { message = $"Invalid subscription type: {request.Type}. Valid types are: Monthly, Semestral, Yearly" });
+        }
+
+        // Validate price
+        if (request.TotalPrice <= 0)
+        {
+            return BadRequest(new { message = "Total price must be greater than 0" });
+        }
+
+        var subscription = new Subscription
+        {
+            Id = Guid.NewGuid(),
+            Type = subscriptionType,
+            TotalPrice = request.TotalPrice,
+            IsDeleted = false
+        };
+        
         _context.Subscriptions.Add(subscription);
         await _context.SaveChangesAsync();
 
         return CreatedAtAction(nameof(GetSubscription), new { id = subscription.Id }, subscription);
     }
-
+    
     // Update subscription
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateSubscription(Guid id, Subscription subscription)
