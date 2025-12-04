94c804d4c1915306c3d06e2b58b2f9436935932f
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-04 00:40:06 +0000

    feat(UserSubscription): Add CreatedAt and UpdatedAt timestamps to UserSubscriptions table

--- PowerUpAPI/PowerUp/Migrations/20251203234548_AddUserSubscriptionTimestamps.Designer.cs
+++ PowerUpAPI/PowerUp/Migrations/20251203234548_AddUserSubscriptionTimestamps.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PowerUp.Data;
 
@@ -11,9 +12,11 @@ using PowerUp.Data;
 namespace PowerUp.Migrations
 {
     [DbContext(typeof(PowerUpDbContext))]
-    partial class PowerUpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251203234548_AddUserSubscriptionTimestamps")]
+    partial class AddUserSubscriptionTimestamps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PowerUpAPI/PowerUp/Migrations/20251203234548_AddUserSubscriptionTimestamps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PowerUp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSubscriptionTimestamps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserSubscriptions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserSubscriptions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "UserSubscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserSubscriptions");
+        }
+    }
+}
