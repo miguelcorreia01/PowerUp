6c12c4ef7f1b508e5f9ad1bf999afdd108608661
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-04 14:11:58 +0000

    feat(Routing): implement route guards for access control

--- PowerUpUI/src/app/app.routes.ts
+++ PowerUpUI/src/app/app.routes.ts
@@ -9,18 +9,59 @@ import { Profile } from './features/profile/pages/profile/profile';
 import { AdminDashboard } from './features/admin-dashboard/pages/admin-dashboard/admin-dashboard';
 import { Users } from './features/users/pages/users/users';
 import { AdminMembership } from './features/admin-membership/pages/admin-membership/admin-membership';
+import { authGuard } from './core/auth/guards/auth.guard';
+import { adminGuard, instructorOrMemberGuard, memberGuard } from './core/auth/guards/role.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'personal-training', component: PersonalTraining },
-  { path: 'group-classes', component: GroupClasses },
-  { path: 'membership', component: Membership },
-  { path: 'profile', component: Profile },
-  { path: 'profile/:id', component: Profile },
-  {path: 'admin-dashboard', component: AdminDashboard },
-  {path: 'users', component: Users },
-  {path: 'admin-membership', component: AdminMembership }
+  
+  { 
+    path: 'dashboard', 
+    component: Dashboard,
+    canActivate: [authGuard]
+  },
+  { 
+    path: 'personal-training', 
+    component: PersonalTraining,
+    canActivate: [authGuard, instructorOrMemberGuard] 
+  },
+  { 
+    path: 'group-classes', 
+    component: GroupClasses,
+    canActivate: [authGuard, instructorOrMemberGuard]
+  },
+  { 
+    path: 'membership', 
+    component: Membership,
+    canActivate: [authGuard, memberGuard]
+  },
+  { 
+    path: 'profile', 
+    component: Profile,
+    canActivate: [authGuard]
+  },
+  { 
+    path: 'profile/:id', 
+    component: Profile,
+    canActivate: [authGuard]
+  },
+  
+  // Admin-only routes
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboard,
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'users',
+    component: Users,
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'admin-membership',
+    component: AdminMembership,
+    canActivate: [authGuard, adminGuard]
+  }
 ];
